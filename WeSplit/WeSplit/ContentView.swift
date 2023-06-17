ab660c059f98f8cc6c5d34990e78df41cde0872c
Yazan Ghunaim <yazanghunaim07@gmail.com>
2023-06-17 10:20:02 +0200

    weSplit app done

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount: Double? = nil
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var isFocused: Bool
+    
+    let tipPercentages = [0,10,15,20,25]
+    
+    var totalPerPerson: Double {
+        let peopleNo = Double(numberOfPeople + 2)
+        let tipVal = Double (tipPercentage)
+        
+        return (checkAmount ?? 0.0 + (checkAmount ?? 0.0 / 100 * tipVal)) / peopleNo
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format:
+                            .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                    .focused($isFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<11) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Tip")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format:
+                            .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar { // modifier which lets us specify toolbars for the view
+                ToolbarItemGroup(placement: .keyboard) { // placing toolbars on keyboard
+                    Spacer() // flexible space push views depending on where we use them
+                    
+                    Button("Done") {
+                        isFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
