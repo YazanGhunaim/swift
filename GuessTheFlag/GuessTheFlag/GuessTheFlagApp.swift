2c3aa924ea669502782981169b0614470821d0f0
Yazan Ghunaim <yazanghunaim07@gmail.com>
2023-06-19 15:31:46 +0200

    guessTheflag files

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Yazan Ghunaim on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
