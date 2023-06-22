90114d6583d7fe42cdfa3d824c8691baa6222aab
Yazan Ghunaim <yazanghunaim07@gmail.com>
2023-06-22 23:05:47 +0200

    files views and modifiers

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Yazan Ghunaim on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
