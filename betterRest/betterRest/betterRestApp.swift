af4da29ebd6a33829e58d65fc113a6a49fbef75d
Yazan Ghunaim <yazanghunaim07@gmail.com>
2023-07-04 10:56:07 +0200

    betterRest UI

---
+++ betterRest/betterRest/betterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterRestApp.swift
+//  betterRest
+//
+//  Created by Yazan Ghunaim on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct betterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
