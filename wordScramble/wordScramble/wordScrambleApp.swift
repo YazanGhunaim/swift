854206f1bf1d3d854c9eaf01a36c0ed60a5b2bcd
Yazan Ghunaim <yazanghunaim07@gmail.com>
2023-07-08 17:18:12 +0200

    explore view + userRow view component

---
+++ wordScramble/wordScramble/wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleApp.swift
+//  wordScramble
+//
+//  Created by Yazan Ghunaim on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
