cf3dbe171725e8ab060ea8db581c5f0b5d6a1a34
Yazan Ghunaim <yazanghunaim07@gmail.com>
2023-07-25 19:00:07 +0200

    day 35

---
+++ edutainment/edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Yazan Ghunaim on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
