863b68d06e5543d1c91a0ef00f6360b8fd2beb0e
Yazan Ghunaim <yazanghunaim07@gmail.com>
2023-08-31 15:21:39 +0300

    day 49+50

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Yazan Ghunaim on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
