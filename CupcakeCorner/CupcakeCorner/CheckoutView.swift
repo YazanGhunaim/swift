15e221f0eff7ba5eb887e0a720e2c1fc10ac74ac
Yazan Ghunaim <yazanghunaim07@gmail.com>
2023-09-01 11:13:10 +0300

    day 51+52

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    @State private var showingError = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -25,12 +29,48 @@ struct CheckoutView: View {
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place Order", action: {})
-                    .padding()
+                Button("Place Order") {
+                    Task { await placeOrder() }
+                }
+                .padding()
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") {}
+        } message: {
+            Text(confirmationMessage)
+        }
+        .alert("Oops..", isPresented: $showingError) {
+            Button("OK") {}
+        } message: {
+            Text("Failed to place order")
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cup cakes is on its way!"
+            
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+            showingError = true
+        }
     }
 }
 
