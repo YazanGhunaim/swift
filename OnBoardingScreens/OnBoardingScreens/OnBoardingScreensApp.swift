0886ccd4c24ee542c4949f0a921d1eab269a915d
Yazan Ghunaim <yazanghunaim07@gmail.com>
2024-01-26 13:06:33 +0100

    reformatting

---
+++ OnBoardingScreens/OnBoardingScreens/OnBoardingScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingScreensApp.swift
+//  OnBoardingScreens
+//
+//  Created by Yazan Ghunaim on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
